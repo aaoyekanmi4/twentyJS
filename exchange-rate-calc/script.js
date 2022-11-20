2ce94b4e9e38fba982a93c4c09d3563d58182951
Arit Oyekan <aaoyekanmi4@gmail.com>
2022-11-19 20:18:24 -0600

    .then version of api call

--- exchange-rate-calc/script.js
+++ exchange-rate-calc/script.js
@@ -6,11 +6,28 @@ const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
 
 // Fetch exchange rates and update the DOM
-function calculate () { }
+function calculate () {
+    let currencyOne = currencyEl_one.value
+    let currencyTwo = currencyEl_two.value
+    fetch(` https://v6.exchangerate-api.com/v6/fdeee77eb69e88c28b69461a/latest/${currencyOne}`)
+        .then(res => res.json())
+        .then(data => {
+            const rate = data.conversion_rates[currencyTwo]
+            rateEl.innerText = `1 ${currencyOne} = ${rate} ${currencyTwo}`
+            amountEl_two.value = (rate * amountEl_one.value).toFixed(2)
+        })
+}
 
 currencyEl_one.addEventListener('change', calculate)
 amountEl_one.addEventListener('input', calculate)
 currencyEl_two.addEventListener('change', calculate)
 amountEl_two.addEventListener('change', calculate)
+
+swap.addEventListener('click', () => {
+    const temp = currencyEl_one.value
+    currencyEl_one.value = currencyEl_two.value
+    currencyEl_two.value = temp
+    calculate()
+})
 
 calculate()
