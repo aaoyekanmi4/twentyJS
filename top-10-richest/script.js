a52d8eb61189a94876d89683309df36344752b51
Arit Oyekan <aaoyekanmi4@gmail.com>
2022-11-21 22:58:33 -0600

    drag and drop done

--- top-10-richest/script.js
+++ top-10-richest/script.js
@@ -51,6 +51,67 @@ function createList () {
             listItems.push(listItem)
             draggableList.appendChild(listItem)
         })
+    addEventListeners()
 }
+
+function checkList () {
+    listItems.forEach((item, index) => {
+        const personName = item.querySelector('.draggable').innerText.trim()
+        if (richestPeople[index] !== personName) {
+            item.classList.add('wrong')
+        } else {
+            item.classList.remove('wrong')
+            item.classList.add('right')
+        }
+    })
+}
+
+function swapItems (fromIndex, toIndex) {
+    const itemOne = listItems[fromIndex].querySelector('.draggable')
+    const itemTwo = listItems[toIndex].querySelector('.draggable')
+    listItems[fromIndex].appendChild(itemTwo)
+    listItems[toIndex].appendChild(itemOne)
+
+}
+
+function dragStart () {
+    dragStartIndex = +this.closest('li').getAttribute('data-index')
+    console.log(dragStartIndex)
+}
+
+function dragEnter () {
+    this.classList.add('over')
+}
+
+function dragLeave () {
+    this.classList.remove('over')
+}
+
+function dragOver (e) {
+    e.preventDefault();
+}
+
+function dragDrop () {
+    const dragEndIndex = +this.getAttribute('data-index');
+    swapItems(dragStartIndex, dragEndIndex)
+}
+
+function addEventListeners () {
+    const draggables = document.querySelectorAll('.draggable')
+    const dragListItems = document.querySelectorAll('.draggable-list li')
+    draggables.forEach((draggable) => {
+        draggable.addEventListener('dragstart', dragStart)
+    })
+
+    dragListItems.forEach(item => {
+        item.addEventListener('dragover', dragOver)
+        item.addEventListener('drop', dragDrop)
+        item.addEventListener('dragenter', dragEnter)
+        item.addEventListener('dragleave', dragLeave)
+    })
+}
+
+checkBtn.addEventListener('click', checkList)
+
 
 createList()
